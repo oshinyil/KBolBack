9f6e747cb706063a3c864878bfabe160d7be28d3
Tane Yoroshi <taneyoroshi@gmail.com>
2016-12-30 07:54:10 +0800

    Added initial web project.

---
+++ KBolBack.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KBolBack.Web.MvcApplication" Language="C#" %>
---
+++ KBolBack.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KBolBack.Web.Startup))]
+namespace KBolBack.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
